2b669034ae706d61ed144a0a6b63561de24071a7
shn07082 <127087415+shn07082@users.noreply.github.com>
2024-05-17 18:06:28 +0900

    Add files via upload

---
+++ javascript/JAVASCRIPT/Area.js
@@ -0,0 +1,5 @@
+function activateResults() {
+    document.getElementById('lodging-card').classList.remove('disabled');
+    document.getElementById('restaurant-card').classList.remove('disabled');
+    document.getElementById('attraction-card').classList.remove('disabled');
+}
