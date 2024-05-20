d5e9c4978ca62bd088191ac1c67a207f6e9eaa7e
shn07082 <127087415+shn07082@users.noreply.github.com>
2024-05-20 09:24:33 +0900

    지역 드롭다운

---
+++ javascript/JAVASCRIPT/AreaDropdown.js
@@ -0,0 +1,20 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("region-select");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.region-button button')) {
+        var dropdowns = document.getElementsByClassName("region-select");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
