74f2a0b0d9b02570e58a0a97d2d813f1d15b4afe
choisong-yi <65282043+choisong-yi@users.noreply.github.com>
2024-05-21 14:29:08 +0900

    Delete javascript/JAVASCRIPT directory

--- javascript/JAVASCRIPT/Area.js
+++
@@ -1,5 +0,0 @@
-function activateResults() {
-    document.getElementById('lodging-card').classList.remove('disabled');
-    document.getElementById('restaurant-card').classList.remove('disabled');
-    document.getElementById('attraction-card').classList.remove('disabled');
-}
